6cf2309826ee58acdc250eed48f8f701c887bb4b
ampunkboyx <85691047+ampunkboyx@users.noreply.github.com>
2023-06-18 00:12:48 +0800

    test

--- app.js
+++ app.js
@@ -17,37 +17,37 @@ let products = [
     {
         id: 1,
         name: 'PAINT SERVICE',
-        image: '1.PNG',
+        image: 'airbrush.PNG',
         price: 120000
     },
     {
         id: 2,
-        name: 'PRODUCT NAME 2',
-        image: '2.PNG',
+        name: 'AIRCOND SERVICE',
+        image: 'aircond.PNG',
         price: 120000
     },
     {
         id: 3,
-        name: 'PRODUCT NAME 3',
-        image: '3.PNG',
+        name: 'BATTERY REPLACEMENT',
+        image: 'battery.PNG',
         price: 220000
     },
     {
         id: 4,
-        name: 'PRODUCT NAME 4',
-        image: '4.PNG',
+        name: 'CAR DIAGNOSTIC',
+        image: 'diagnostic.PNG',
         price: 123000
     },
     {
         id: 5,
-        name: 'PRODUCT NAME 5',
-        image: '5.PNG',
+        name: 'CHANGE OIL & FILTER',
+        image: 'engine-oil.PNG',
         price: 320000
     },
     {
         id: 6,
-        name: 'PRODUCT NAME 6',
-        image: '6.PNG',
+        name: 'TIRE INSTALLATION',
+        image: 'spare-tire.PNG',
         price: 120000
     }
 ];
