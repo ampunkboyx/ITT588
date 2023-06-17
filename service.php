880c2cf26b82e4ef28189902f71fd4155d023b14
ampunkboyx <85691047+ampunkboyx@users.noreply.github.com>
2023-06-17 18:35:04 +0800

    simple design

--- service.php
+++ service.php
@@ -1,10 +1,25 @@
-<!DOCTYPE this is page of services that has been offer>
+<!DOCTYPE html>
 <html>
-    
-    <body>
-
-        <h1>Service</h1>
-
-        
-    </body>
+  <head>
+    <title>Workshop Website</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+  </head>
+  <body>
+    <div id="container">
+        <header>
+            <h1>Workshop Website</h1>
+            <nav>
+                <button type="button" onclick="window.location='index.php';">Home</button>
+                <button type="button" onclick="window.location='service.php';">Service</button>
+                <button type="button" onclick="window.location='about.php';">About</button>
+            </nav>
+        </header>
+        <div id="content">
+            <h1>Service</h1>
+        </div>
+        <footer>
+            <p>&copy; 2023 Workshop Website. All rights reserved.</p>
+        </footer>
+    </div>
+  </body>
 </html>
