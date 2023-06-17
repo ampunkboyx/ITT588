e22d596408d0c9a7655ebc92ab4bbdef82cc5b2c
ampunkboyx <85691047+ampunkboyx@users.noreply.github.com>
2023-06-18 02:01:46 +0800

    Revert "psuh"
    This reverts commit 263ad01078fcb53c021f321488de1bca77f8d8dd.

--- app.js
+++ app.js
@@ -6,13 +6,12 @@ let body = document.querySelector('body');
 let total = document.querySelector('.total');
 let quantity = document.querySelector('.quantity');
 
-openShopping.addEventListener('click', () => {
+openShopping.addEventListener('click', ()=>{
     body.classList.add('active');
-});
-
-closeShopping.addEventListener('click', () => {
+})
+closeShopping.addEventListener('click', ()=>{
     body.classList.remove('active');
-});
+})
 
 let products = [
     {
@@ -76,7 +75,8 @@ function initApp() {
                 </div>`;
         }
         list.appendChild(newDiv);
-
+        
+        // Set default color to black if dropdown exists
         if (value.colors) {
             let dropdown = newDiv.querySelector('.color-dropdown');
             dropdown.value = 'Black';
@@ -84,25 +84,42 @@ function initApp() {
     });
 }
 
+
 function generateColorOptions(colors) {
     let options = '';
     colors.forEach((color) => {
-        options += `<option value="${color}">${color}</option>`;
+        options += `<option value="${color}" style="color: ${color.toLowerCase()}">${color}</option>`;
     });
     return options;
 }
 
 function changeColor(key, color) {
     listCards[key].color = color;
-    let itemCard = listCard.children[key];
-    let colorPreview = itemCard.querySelector('.color-preview');
-    colorPreview.style.backgroundColor = color;
+    
+    // Update the image based on the selected color
+    let imageElement = document.querySelector(`.item:nth-child(${key + 1}) img`);
+    if (imageElement) {
+        let imageName = products[key].image;
+        let colorImageName = imageName.replace('.PNG', `_${color.toLowerCase()}.PNG`);
+        imageElement.src = `image/${colorImageName}`;
+    }
+    
+    // Update the color preview
+    let colorPreview = document.querySelector(`.item:nth-child(${key + 1}) .color-preview`);
+    if (colorPreview) {
+        colorPreview.style.backgroundColor = color;
+    }
+    
+    // Update the cart if the product is already in the cart
+    reloadCard();
 }
+
 
 initApp();
 
 function addToCard(key) {
     if (listCards[key] == null) {
+        // Copy product from list to list card
         listCards[key] = JSON.parse(JSON.stringify(products[key]));
         listCards[key].quantity = 1;
         if (listCards[key].colors) {
@@ -121,8 +138,10 @@ function reloadCard() {
         count = count + value.quantity;
         if (value != null) {
             let newDiv = document.createElement('li');
+            let imageName = products[key].image;
+            let colorImageName = imageName.replace('.PNG', value.color ? `_${value.color.toLowerCase()}.png` : '.PNG');
             newDiv.innerHTML = `
-                <div><img src="image/${value.image}"/></div>
+                <div><img src="image/${colorImageName}"/></div>
                 <div>${value.name}${value.color ? ` - ${value.color}` : ''}</div>
                 <div>${value.price.toLocaleString()}</div>
                 <div>
@@ -131,27 +150,18 @@ function reloadCard() {
                     <button onclick="changeQuantity(${key}, ${value.quantity + 1})">+</button>
                 </div>`;
             listCard.appendChild(newDiv);
-            let colorPreview = newDiv.querySelector('.color-preview');
-            if (value.color) {
-                colorPreview.style.backgroundColor = value.color;
-            }
         }
-    });
-
+    })
     total.innerText = totalPrice.toLocaleString();
     quantity.innerText = count;
 }
 
-function changeQuantity(key, quantity) {
-    if (quantity == 0) {
+function changeQuantity(key, quantity){
+    if(quantity == 0){
         delete listCards[key];
-    } else {
+    }else{
         listCards[key].quantity = quantity;
         listCards[key].price = quantity * products[key].price;
     }
     reloadCard();
-}
-
-function redirectToCheckout() {
-    window.location.href = "checkout.html";
 }
