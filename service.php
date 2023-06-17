65f384477dd86dd51075da7d462ad8f410d64494
ampunkboyx <85691047+ampunkboyx@users.noreply.github.com>
2023-06-17 20:50:32 +0800

    Update service.php

--- service.php
+++ service.php
@@ -11,12 +11,30 @@
             <button type="button" onclick="window.location='index.php';">Home</button>
             <button type="button" onclick="window.location='service.php';">Service</button>
             <button type="button" onclick="window.location='about.php';">About</button>
-            
         </header>
         <div id="content">
             <h2>Welcome to Our Workshop</h2>
             <p>At our workshop, we offer a wide range of services to keep your vehicle running smoothly.</p>
             <p>Choose from the following services:</p>
+            <br>
+            <div id=
+            <input type="checkbox" id="myCheckbox1" />
+            <label for="myCheckbox1"><img src="icon\airbrush.png" alt="Black Icon" width="200" height="200"><p>Airbrush</p></label>
+
+            <input type="checkbox" id="myCheckbox2" />
+            <label for="myCheckbox2"><img src="icon\aircond.png" alt="Black Icon" width="200" height="200"><p>Airbrush</p></label>
+
+            <input type="checkbox" id="myCheckbox3" />
+            <label for="myCheckbox3"><img src="icon\battery.png" alt="Black Icon" width="200" height="200"><p>Airbrush</p></label>
+            <br>
+            <input type="checkbox" id="myCheckbox4" />
+            <label for="myCheckbox4"><img src="icon\diagnostic.png" alt="Black Icon" width="200" height="200"><p>Airbrush</p></label>
+
+            <input type="checkbox" id="myCheckbox5" />
+            <label for="myCheckbox5"><img src="icon\engine-oil.png" alt="Black Icon" width="200" height="200"><p>Airbrush</p></label>
+
+            <input type="checkbox" id="myCheckbox6" />
+            <label for="myCheckbox6"><img src="icon\spare-tire.png" alt="Black Icon" width="200" height="200"><p>Airbrush</p></label>
         </div>
         <footer>
             <p>&copy; 2023 Workshop Website. All rights reserved.</p>
