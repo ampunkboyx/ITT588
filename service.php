fa0b643baaef805a73ba35aaf57c3bf2d15aa549
Aqid Ishak <aqidishak28@gmail.com>
2023-06-17 20:56:08 +0800

    Update service.php

--- service.php
+++ service.php
@@ -22,19 +22,19 @@
             <label for="myCheckbox1"><img src="icon\airbrush.png" alt="Black Icon" width="200" height="200"><p>Airbrush</p></label>
 
             <input type="checkbox" id="myCheckbox2" />
-            <label for="myCheckbox2"><img src="icon\aircond.png" alt="Black Icon" width="200" height="200"><p>Airbrush</p></label>
+            <label for="myCheckbox2"><img src="icon\aircond.png" alt="Black Icon" width="200" height="200"><p>Air Cond</p></label>
 
             <input type="checkbox" id="myCheckbox3" />
-            <label for="myCheckbox3"><img src="icon\battery.png" alt="Black Icon" width="200" height="200"><p>Airbrush</p></label>
+            <label for="myCheckbox3"><img src="icon\battery.png" alt="Black Icon" width="200" height="200"><p>Battery Change</p></label>
             <br>
             <input type="checkbox" id="myCheckbox4" />
-            <label for="myCheckbox4"><img src="icon\diagnostic.png" alt="Black Icon" width="200" height="200"><p>Airbrush</p></label>
+            <label for="myCheckbox4"><img src="icon\diagnostic.png" alt="Black Icon" width="200" height="200"><p>Car Inspection</p></label>
 
             <input type="checkbox" id="myCheckbox5" />
-            <label for="myCheckbox5"><img src="icon\engine-oil.png" alt="Black Icon" width="200" height="200"><p>Airbrush</p></label>
+            <label for="myCheckbox5"><img src="icon\engine-oil.png" alt="Black Icon" width="200" height="200"><p>Oil Change</p></label>
 
             <input type="checkbox" id="myCheckbox6" />
-            <label for="myCheckbox6"><img src="icon\spare-tire.png" alt="Black Icon" width="200" height="200"><p>Airbrush</p></label>
+            <label for="myCheckbox6"><img src="icon\spare-tire.png" alt="Black Icon" width="200" height="200"><p>Tyre</p></label>
         </div>
         <footer>
             <p>&copy; 2023 Workshop Website. All rights reserved.</p>
